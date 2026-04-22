7d0b9541d95eb13aff2671f8fef3049fe01d9593
BRIAN ANDRES ARAVENA QUEZADA <163011832+brx2b@users.noreply.github.com>
2026-04-22 17:45:30 +0000

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
-import DonatonLogo from "./assets/marca/Donaton.png";
-
+import "./Components/TopBar.jsx"
+import TopBar from "./Components/TopBar.jsx";
 export default function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      
+      <TopBar/>
     </>
   );
 }
---
+++ src/Components/TopBar.jsx
@@ -0,0 +1,14 @@
+import "../App.css";
+export default function TopBar(){
+    return(
+        <>
+        <div id="TopBar">
+            <h1 id="logo">Donaton</h1>
+            <section>
+                <label>Inicio</label>
+                <label>Solicitudes</label>
+            </section>
+        </div>
+        </>
+    )
+}
