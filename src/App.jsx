6590030249557b38220c73eaad2d5cbf2714cf06
Brx2b <briansnowshine@gmail.com>
2026-04-22 01:34:22 -0400

    Inicio

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
+import DonatonLogo from "./assets/marca/Donaton.png";
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <section id="logo">
+        <h1>Donaton</h1>
+      </section>
+    </>
+  );
 }
-
-export default App;
