7a833839c2a8ab193c379628e20693aca44aa338
BRIAN ANDRES ARAVENA QUEZADA <163011832+brx2b@users.noreply.github.com>
2026-04-22 18:04:29 +0000

    inicio topbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import "./Components/TopBar.jsx"
 import TopBar from "./Components/TopBar.jsx";
+
 export default function App() {
   const [count, setCount] = useState(0);
 
--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -7,6 +7,12 @@ export default function TopBar(){
             <section>
                 <label>Inicio</label>
                 <label>Solicitudes</label>
+                <label>Sedes</label>
+                <label>Nosotros</label>
+            </section>
+            <section id="login">
+                <label>Login</label>
+                <label>Registro</label>
             </section>
         </div>
         </>
