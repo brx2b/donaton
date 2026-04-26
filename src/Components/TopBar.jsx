44f18da9268ec5fd103d12945505689b848ae4a5
Brx2b <briansnowshine@gmail.com>
2026-04-26 19:42:19 -0400

    cambios topbar

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -1,20 +1,20 @@
 import "../App.css";
-export default function TopBar(){
-    return(
-        <>
-        <div id="TopBar">
-            <h1 id="logo">Donaton</h1>
-            <section>
-                <label>Inicio</label>
-                <label>Solicitudes</label>
-                <label>Sedes</label>
-                <label>Nosotros</label>
-            </section>
-            <section id="login">
-                <label>Login</label>
-                <label>Registro</label>
-            </section>
-        </div>
-        </>
-    )
+export default function TopBar() {
+  return (
+    <>
+      <div id="TopBar">
+        <h1 id="logo">Donaton</h1>
+        <section id="opciones">
+          <label>Inicio</label>
+          <label>Solicitudes</label>
+          <label>Sedes</label>
+          <label>Nosotros</label>
+        </section>
+        <section id="login">
+          <label>Login</label>
+          <label>Registro</label>
+        </section>
+      </div>
+    </>
+  );
 }
