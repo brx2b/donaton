548947991fb3613e4d7b72bd4a813cea10190b75
BRIAN ANDRES ARAVENA QUEZADA <163011832+brx2b@users.noreply.github.com>
2026-05-06 17:37:22 +0000

    solicitudes avances

--- src/Components/pages/Solicitudes.jsx
+++ src/Components/pages/Solicitudes.jsx
@@ -15,5 +15,55 @@ export default function Solicitudes() {
       </>
     );
   }
-  return <h1>Solicitudes</h1>;
+
+  const buscarSolicitudes = "";
+  const solicitudesPendientes = [];
+
+  const handleSolicitudes = () => {
+    console.log("Cargando solicitudes");
+    for (let i = 0; i < solicitudesPendientes.length; i++) {
+      const solicitud = solicitudesPendientes[i];
+    }
+    console.log("solicitud cargadas");
+  };
+  return (
+    <>
+      <h1 id="headerSolicitudes">Solicitudes de ayuda humanitaria</h1>
+      {/* pasar como prompts desde la base de datos informacion de las solicitudes en espera */}
+      {/*  */}
+      <div onClick={handleSolicitudes} id="ListaSolicitudes">
+        <div className="contenedorSolicitud">
+          <h5>Solicitud:</h5>
+          <p id="descSolicitud"> </p>
+          <h5>Sede:</h5>
+          <p1 id="sedeSolicitud"> </p1>
+          <h5>Realizado por:</h5>
+          <button id="botonDetalles">Más detalles</button>
+        </div>
+
+        <div className="contenedorSolicitud">
+          <h5>Solicitud:</h5>
+          <p id="descSolicitud"> </p>
+          <h5>Sede:</h5>
+          <p1 id="sedeSolicitud"> </p1>
+          <h5>Realizado por:</h5>
+          <button id="botonDetalles">Más detalles</button>
+        </div>
+
+        <div className="contenedorSolicitud">
+          <h5>Solicitud:</h5>
+          <p id="descSolicitud"> </p>
+          <h5>Sede:</h5>
+          <p1 id="sedeSolicitud"> </p1>
+          <h5>Realizado por:</h5>
+          <button id="botonDetalles">Más detalles</button>
+        </div>
+      </div>
+      <div id="footer">
+        <footer>
+          <p>Sitio web desarrollado por brx2b</p>
+        </footer>
+      </div>
+    </>
+  );
 }
